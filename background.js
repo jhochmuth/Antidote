3ccc017534db09da34e6274e80f82c789d56d87d
jhochmuth <jhochmuth@comcast.net>
2019-07-07 21:15:51 -0600

    Changed styling.

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
-var model, word_indices;
+var toxicityModel, toxicityWordIndices;
 
 chrome.runtime.onInstalled.addListener(async function() {
   chrome.storage.local.set({threshold: .4, active: true});
-  model = await getModel();
-  wordIndices = await getWordIndices();
+  toxicityModel = await getToxicityModel();
+  toxicityWordIndices = await getToxicityWordIndices();
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([
@@ -22,10 +22,10 @@ chrome.runtime.onInstalled.addListener(async function() {
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   var inputBuffer = tf.buffer([1, 60], "float32");
   request.comment.toLowerCase().split(" ").forEach(function(word, index) {
-    wordIndex = wordIndices[word];
+    wordIndex = toxicityWordIndices[word];
     inputBuffer.set(wordIndex, 0, index);
   });
-  var prediction = model.predict(inputBuffer.toTensor()).arraySync()[0][0];
+  var prediction = toxicityModel.predict(inputBuffer.toTensor()).arraySync()[0][0];
 
   sendResponse({prediction: prediction});
 });
--- model.js
+++ model.js
@@ -1,12 +1,22 @@
-const MODEL_PATH = "https://storage.googleapis.com/youtoxic-extension-models/tfjs_models_keras/model.json";
-const INDEX_PATH = "https://storage.googleapis.com/youtoxic-extension-models/model_metadata/word_index.json";
+const TOXICITY_MODEL_PATH = "https://storage.googleapis.com/youtoxic-extension-models/toxicity_model/model.json";
+const TOXICITY_INDEX_PATH = "https://storage.googleapis.com/youtoxic-extension-models/toxicity_model/word_index.json";
 
-async function getModel() {
-  const model = await tf.loadLayersModel(MODEL_PATH);
+async function getToxicityModel() {
+  const model = await tf.loadLayersModel(TOXICITY_MODEL_PATH);
   return model;
 }
 
-async function getWordIndices() {
-  const wordIndices = await $.getJSON(INDEX_PATH);
+async function getToxicityWordIndices() {
+  const wordIndices = await $.getJSON(TOXICITY_INDEX_PATH);
+  return wordIndices;
+}
+
+async function getPoliticalModel() {
+  const model = await tf.loadLayersModel(POLITICAL_MODEL_PATH);
+  return model;
+}
+
+async function getPoliticalWordIndices() {
+  const wordIndices = await $.getJSON(POLITICAL_INDEX_PATH);
   return wordIndices;
 }
